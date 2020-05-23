3d714a1792f0a5a0d4608bf0f79bc52e0beb978c
Mathurin <mathpier305@yahoo.com>
2020-05-23 15:02:12 -0400

    initial commit

---
+++ TheSliderText/TheSliderPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace TheSliderText
+{
+    public partial class TheSliderPage : ContentPage
+    {
+        string[] listOfSentences = {
+            " Sentence # 1 ",
+            " Sentence # 2",
+            " Sentence # 3",
+            " Sentence # 4"
+        };
+        int count = 0;
+        public TheSliderPage()
+        {
+            InitializeComponent();
+
+            slider.Minimum = 16;
+            adesc.Text = listOfSentences[0];
+        }
+
+        void Button_Clicked(System.Object sender, System.EventArgs e)
+        {
+            count += 1;
+            count %= listOfSentences.Length;
+            adesc.Text = listOfSentences[count];
+        }
+     
+    }
+}
